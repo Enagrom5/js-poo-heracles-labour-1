89587ce297b8f0150b9291f854bea3680c777eed
MORGANE DEBARGE <morganedebarge@msn.com>
2023-10-18 12:20:05 +0200

    finish

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 // First Labour : Heracles vs Nemean Lion
 // use your Figher class here
+  const Fighter = require("./src/Fighter.js")
+
+const Heracles = new Fighter("Heracles 🧔",20,6)
+const NemeanLion = new Fighter("Nemean Lion 🦁",11,13)
+
+while (Heracles.life> 0 && NemeanLion.life > 0){
+    Heracles.fight(NemeanLion);
+    NemeanLion.fight(Heracles);
+}
+if (Heracles.life <=0){console.log(`Le lion a vaincu Heracles il lui reste ${NemeanLion.life}PV`)}
+else{console.log(`Heracles a vaincu le lion il lui reste ${Heracles.life}PV`)}
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,25 @@
-/* Fighter class definition */
+const maxLife = 100;
+
+class Fighter {
+  constructor(name, strenght, dexterity) {
+    (this.name = name),
+      (this.strenght = strenght),
+      (this.dexterity = dexterity);
+    this.life = maxLife;
+  }
+  fight(defender) {
+    let degats = Math.ceil(
+      Math.random() * (this.strenght - (1 + 1)) - defender.dexterity
+    );
+
+    if (degats >= 1) {
+      defender.life -= degats;
+      console.log(
+        `${this.name} inflige ${degats} à ${defender.name}, ${defender.name} a encore ${defender.life} PV`
+      );
+    }else{console.log(
+        `${this.name} attaque, ${defender.name} a encore ${defender.life} PV`
+      )}
+  }
+}
+module.exports = Fighter;
